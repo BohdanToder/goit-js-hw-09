76b3c24a92b3c7d34bd45a755980135c1505f7aa
BohdanToder <bohdantoder7454@gmail.com>
2022-02-12 17:39:00 +0200

    Adds first task solution

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,29 @@
+const bodyRef = document.querySelector('body');
+const buttonStartRef = document.querySelector('[data-start]');
+const buttonStopRef = document.querySelector('[data-stop]');
 
+let intervalId = null;
+
+buttonStartRef.addEventListener('click', startInterval);
+buttonStopRef.addEventListener('click', stopInterval);
+
+
+function startInterval () { 
+    intervalId = setInterval(changeBgColor, 1000);
+    buttonStartRef.disabled = true;
+    buttonStopRef.disabled = false;
+}
+
+function stopInterval () { 
+    clearInterval(intervalId);
+    buttonStartRef.disabled = false;
+    buttonStopRef.disabled = true;
+}
+
+function changeBgColor() {
+bodyRef.style.backgroundColor = getRandomHexColor();
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
